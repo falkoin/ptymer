0c4be1f21fc4afc8c116fe12224aa09f7e018789
Falko Döhring <github@falko.in>
2024-02-07 21:49:36 +0100

    feat: add basic timer cli

---
+++ app.py
@@ -0,0 +1,90 @@
+import typer
+import json
+from datetime import datetime, timedelta, date
+from os import path
+
+FILE_NAME = "data.json"
+
+
+def db_file_existing() -> bool:
+    return path.isfile(f"./{FILE_NAME}")
+
+
+def create_file() -> None:
+    with open(FILE_NAME, "w") as file:
+        json.dump({f"{date.today()}": []}, file)
+
+
+def db_load() -> dict:
+    with open(FILE_NAME, "r") as file:
+        return json.load(file)
+
+
+def create_entry(data: dict, today: str) -> dict:
+    if not data.get(today):
+        data[today] = []
+    return data
+
+
+def create_timestamp(data: dict, date_today: str, event: str) -> None:
+    data[date_today].append({"event": event, "time": f"{datetime.now()}"})
+    with open(FILE_NAME, "w+") as file:
+        file.seek(0)
+        json.dump(data, file)
+
+
+def check_state_allowed(data: dict, date_today: str, event: str) -> bool:
+    data_today = data.get(date_today)
+    if data_today:
+        last_event = data_today[-1].get("event")
+        return last_event != event
+    raise Exception("Couldn't find date entry in data file.")
+
+
+def calc_duration(data: dict, date_today: str) -> None:
+    data_for_date = data.get(date_today)
+    if data_for_date:
+        all_times = [entry.get("time") for entry in data_for_date]
+        format_ = "%Y-%m-%d %H:%M:%S.%f"
+        diff_times = [
+            datetime.strptime(x, format_) - datetime.strptime(y, format_)
+            for x, y in zip(all_times[1::2], all_times[::2])
+        ]
+
+        print(f"Worked for {sum(diff_times, timedelta())} hours")
+    else:
+        raise Exception("Couldn't calculate duration for today")
+
+
+app = typer.Typer()
+
+
+@app.command()
+def start():
+    if not db_file_existing():
+        create_file()
+    data = db_load()
+    date_today = f"{date.today()}"
+    if check_state_allowed(data, date_today, "start"):
+        create_entry(data, date_today)
+        create_timestamp(data, date_today, "start")
+    else:
+        print("Session already running.")
+
+
+@app.command()
+def stop():
+    if not db_file_existing():
+        print("No session started, yet")
+    data = db_load()
+    date_today = f"{date.today()}"
+    if check_state_allowed(data, date_today, "stop"):
+        create_entry(data, date_today)
+        create_timestamp(data, date_today, "stop")
+        calc_duration(data, date_today)
+    else:
+        print("Session already stopped.")
+
+
+if __name__ == "__main__":
+    app()
---
+++ tests/test_timer.py
@@ -0,0 +1,73 @@
+from unittest import TestCase
+from unittest.mock import patch
+from app import check_state_allowed, create_entry, db_file_existing
+
+
+class TestCreateEntry(TestCase):
+
+    def setUp(self) -> None:
+        self.data = {"2024-01-01": [{"some_key": "some_value"}]}
+
+    def test_create_entry(self) -> None:
+        # when
+        result = create_entry(self.data, "2024-01-01")
+        # then
+        self.assertEqual(self.data, result)
+
+    def test_create_entry_new_day(self) -> None:
+        # when
+        today = "2024-12-12"
+        result = create_entry(self.data, today)
+        # then
+        self.assertEqual(self.data | {today: []}, result)
+
+
+class TestDb(TestCase):
+
+    def setUp(self) -> None:
+        self.db_file = patch("os.path.isfile").start()
+
+    def test_db_file_existing(self) -> None:
+        # given
+        self.db_file.return_value = True
+        # when
+        result = db_file_existing()
+        # then
+        self.db_file.assert_called_with("./data.json")
+        self.assertEqual(True, result)
+
+    def test_db_file_not_existing(self) -> None:
+        # given
+        self.db_file.return_value = False
+        # when
+        result = db_file_existing()
+        # then
+        self.db_file.assert_called_with("./data.json")
+        self.assertEqual(False, result)
+
+
+class TestState(TestCase):
+
+    def setUp(self) -> None:
+        self.date_today = "2024-01-01"
+
+    def test_check_state_allowed(self):
+        # given
+        data = {self.date_today: [{"event": "start"}, {"event": "stop"}]}
+        # when
+        for event, expected in zip(("start", "stop"), (True, False)):
+            with self.subTest(f"event: {event}"):
+                result = check_state_allowed(data, self.date_today, event)
+                # then
+                self.assertEqual(expected, result)
+
+    def test_check_state_allowed_date_not_found(self):
+        # given
+        data = {"1999-01-01": []}
+        # when
+        with self.assertRaises(Exception) as error:
+            check_state_allowed(data, self.date_today, "start")
+        # then
+        self.assertEqual("Couldn't find date entry in data file.", error.exception.args[0])
+
+
