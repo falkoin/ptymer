61be5b547500a38921062d72173754daf8d88001
Falko Döhring <github@falko.in>
2024-03-03 08:21:54 +0100

    refactor: move database interactions to class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Connection
+from typing import Any
 import typer
 from datetime import datetime, timedelta, date
 from os import path
@@ -9,36 +10,61 @@ FILE_NAME = "ptymer.db"
 FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
+class Database:
+    filename = FILE_NAME
+
+    def __init__(self):
+        if not path.isfile(f"./{self.filename}"):
+            self.con = self.create()
+        self.con = self.load()
+        self.date_today = f"{date.today()}"
+
+    def load(self) -> Connection:
+        return sqlite3.connect(self.filename)
+
+    def create(self) -> Connection:
+        con = self.load()
+        cur = con.cursor()
+        cur.execute("CREATE TABLE timestamp(date, event, time)")
+        return con
+
+    def close(self) -> None:
+        self.con.close()
+
+    def create_timestamp(self, event: str, delta: int = 0) -> None:
+        cur = self.con.cursor()
+        time_stamp = self._calc_time_stamp(delta)
+        cur.execute(
+            f"INSERT INTO timestamp VALUES ('{self.date_today}', '{event}', '{time_stamp}')"
+        )
+        self.con.commit()
+
+    @staticmethod
+    def _calc_time_stamp(delta: int) -> str:
+        return (datetime.now() - timedelta(minutes=delta)).strftime(FORMAT)
+
+    def get_times_by(self, event: str, ascending: bool = True) -> list[Any]:
+        cur = self.con.cursor()
+        order = "ASC" if ascending else "DESC"
+        return cur.execute(
+            f"SELECT time FROM timestamp WHERE date='{self.date_today}' AND event='{event}' ORDER BY time {order}"
+        ).fetchall()
+
+    def get_last_event(self) -> str | None:
+        cur = self.con.cursor()
+        return cur.execute(
+            f"SELECT event FROM timestamp WHERE date='{self.date_today}' ORDER BY time DESC"
+        ).fetchone()
+
+
 def db_file_existing() -> bool:
     return path.isfile(f"./{FILE_NAME}")
 
 
-def db_create() -> None:
-    con = sqlite3.connect(FILE_NAME)
-    cur = con.cursor()
-    cur.execute("CREATE TABLE timestamp(date, event, time)")
-
-
-def db_load() -> Connection:
-    return sqlite3.connect(FILE_NAME)
-
-
-def create_timestamp(con: Connection, date_today: str, event: str, delta: int) -> None:
-    time_stamp = (datetime.now() - timedelta(minutes=delta)).strftime(FORMAT)
-    cur = con.cursor()
-    cur.execute(
-        f"INSERT INTO timestamp VALUES ('{date_today}', '{event}', '{time_stamp}')"
-    )
-    con.commit()
-
-
-def check_state_allowed(con: Connection, date_today: str, event: str) -> bool:
-    cur = con.cursor()
-    last_event = cur.execute(
-        f"SELECT event FROM timestamp WHERE date='{date_today}' ORDER BY time DESC"
-    )
-    if last_event:
-        return last_event != event
+def check_state_allowed(db: Database, event: str) -> bool:
+    last_event = db.get_last_event()
+    if last_event is not None:
+        return last_event[0] != event
     return True
 
 
@@ -48,15 +74,9 @@ def output_with_timestamp(text: str, delta: int = 0) -> None:
     print(f"[{time_stamp}]: " + text)
 
 
-def calc_worktime(con: Connection, date_today: str) -> timedelta:
-    cur = con.cursor()
-    times_start = cur.execute(
-        f"SELECT time FROM timestamp WHERE date='{date_today}' AND event='start' ORDER BY time ASC"
-    ).fetchall()
-
-    times_stop = cur.execute(
-        f"SELECT time FROM timestamp WHERE date='{date_today}' AND event='stop' ORDER BY time ASC"
-    ).fetchall()
+def calc_worktime(db: Database) -> timedelta:
+    times_start = db.get_times_by(event="start", ascending=True)
+    times_stop = db.get_times_by(event="stop", ascending=True)
 
     if times_start:
         if len(times_start) > len(times_stop):
@@ -68,10 +88,10 @@ def calc_worktime(con: Connection, date_today: str) -> timedelta:
 
 def calc_duration(time_1: list[tuple], time_2: list[tuple]) -> timedelta:
     diff_times = [
-            datetime.strptime(x[0], FORMAT) - datetime.strptime(y[0], FORMAT)
-            for x, y in zip(time_1, time_2)
-            ]
-    
+        datetime.strptime(x[0], FORMAT) - datetime.strptime(y[0], FORMAT)
+        for x, y in zip(time_1, time_2)
+    ]
+
     return sum(diff_times, timedelta())
 
 
@@ -79,33 +99,37 @@ app = typer.Typer()
 
 
 @app.command()
-def start(delta: Annotated[int, typer.Option(help="Time delta in minutes to start in the past.")] = 0):
-    if not db_file_existing():
-        db_create()
-    date_today = f"{date.today()}"
-    con = db_load()
-    if check_state_allowed(con, date_today, "start"):
-        create_timestamp(con, date_today, "start", delta)
+def start(
+    delta: Annotated[
+        int, typer.Option(help="Time delta in minutes to start in the past.")
+    ] = 0
+):
+    db = Database()
+    if check_state_allowed(db, "start"):
+        db.create_timestamp("start", delta)
         output_with_timestamp("Started working", delta)
     else:
         print("Session already running.")
-    con.close()
+    db.close()
 
 
 @app.command()
-def stop(delta: Annotated[int, typer.Option(help="Time delta in minutes to start in the past.")] = 0):
+def stop(
+    delta: Annotated[
+        int, typer.Option(help="Time delta in minutes to stop in the past.")
+    ] = 0
+):
     if not db_file_existing():
         print("No session started, yet")
         return
-    date_today = f"{date.today()}"
-    con = db_load()
-    if check_state_allowed(con, date_today, "stop"):
-        create_timestamp(con, date_today, "stop", delta)
-        duration = calc_worktime(con, date_today)
+    db = Database()
+    if check_state_allowed(db, "stop"):
+        db.create_timestamp("stop", delta)
+        duration = calc_worktime(db)
         output_with_timestamp(f"Worked for {duration} hours", delta)
     else:
         print("Session already stopped.")
-    con.close()
+    db.close()
 
 
 @app.command()
@@ -113,11 +137,10 @@ def show():
     if not db_file_existing():
         print("No data to show")
         return
-    date_today = f"{date.today()}"
-    con = db_load()
-    duration = calc_worktime(con, date_today)
+    db = Database()
+    duration = calc_worktime(db)
     output_with_timestamp(f"Worked for {duration} hours")
-    con.close()
+    db.close()
 
 
 if __name__ == "__main__":
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,16 +1,13 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, call, patch
 from app import (
+    Database,
     calc_duration,
-    db_create,
     check_state_allowed,
     db_file_existing,
-    db_load,
-    create_timestamp,
     output_with_timestamp,
 )
 from datetime import datetime, date, timedelta
-from sqlite3 import Connection
 
 
 class TestDb(TestCase):
@@ -18,6 +15,7 @@ class TestDb(TestCase):
     def setUp(self) -> None:
         self.db_file = patch("os.path.isfile").start()
         self.filename = "ptymer.db"
+        self.db = MagicMock(spec=Database)
 
     def test_db_file_existing(self) -> None:
         # given
@@ -40,8 +38,9 @@ class TestDb(TestCase):
     def test_db_create(self) -> None:
         # given
         sqlite = patch("app.sqlite3").start()
+        patch("app.path.isfile", return_value=False).start()
         # when
-        db_create()
+        _ = Database()
         # then
         sqlite.assert_has_calls(
             [
@@ -53,42 +52,71 @@ class TestDb(TestCase):
             ]
         )
 
+    def test_db_close(self) -> None:
+        # given
+        sqlite = patch("app.sqlite3").start()
+        db = Database()
+        # when
+        db.close()
+        # then
+        sqlite.assert_has_calls([call.connect().close()])
+
     def test_db_load(self) -> None:
         # given
         con = patch("app.sqlite3.connect", return_value={}).start()
         # when
-        result = db_load()
+        db = Database()
         # then
-        self.assertEqual({}, result)
+        self.assertEqual({}, db.con)
         con.assert_called_once_with("ptymer.db")
 
     def test_create_timestamp(self) -> None:
         # given
-        con = patch("app.sqlite3.connect").start()
+        con = patch("app.sqlite3").start()
+        today = patch("app.date", wraps=date).start()
+        today.today.return_value = "2024-01-01"
         now = patch("app.datetime", wraps=datetime).start()
         now.now.return_value = datetime.strptime(
             "2024-01-01 17:00:00", "%Y-%m-%d %H:%M:%S"
         )
-        date_today = "2024-01-01"
+        db = Database()
         expected_call = "INSERT INTO timestamp VALUES ('2024-01-01', 'start', '2024-01-01 17:00:00')"
         # when
-        create_timestamp(con, date_today, "start")
+        db.create_timestamp("start")
         # then
-        con.assert_has_calls([call.cursor(), call.cursor().execute(expected_call)])
+        con.assert_has_calls(
+            [
+                call.connect("ptymer.db"),
+                call.connect().cursor(),
+                call.connect().cursor().execute(expected_call),
+                call.connect().commit(),
+            ]
+        )
 
     def test_check_state_allowed(self) -> None:
         # given
-        mock_con = MagicMock(spec=Connection)
-        mock_con.cursor.return_value.execute.return_value = "start"
-        date_today = "2024-01-01"
+        mock_db = MagicMock(spec=Database)
+        mock_db.get_last_event.side_effect = [("start",), ("start",)]
         for event, expected in zip(("start", "stop"), (False, True)):
             with self.subTest(event):
                 # when
-                result = check_state_allowed(mock_con, date_today, event)
+                result = check_state_allowed(mock_db, event)
                 # then
                 self.assertEqual(expected, result)
 
-    def test_ouput_with_timestamp(self) -> None:
+    def test_calc_time_stamp(self) -> None:
+        # given
+        now = patch("app.datetime", wraps=datetime).start()
+        now.now.return_value = datetime.strptime(
+            "2024-01-01 17:00:00", "%Y-%m-%d %H:%M:%S"
+        )
+        db = Database
+        # when
+        time_stamp = db._calc_time_stamp(delta=10)
+        # then
+        self.assertEqual("2024-01-01 16:50:00", time_stamp)
+
+    def test_output_with_timestamp(self) -> None:
         # given
         std_out = patch("builtins.print").start()
         today = patch("app.date", wraps=datetime).start()
